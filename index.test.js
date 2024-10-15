fe694a0403780c3ea4d017aef1fb2f0b4dd08639
Dhamu2955 <Dharmendra.Pindolia@gmail.com>
2024-10-15 10:50:49 +0100

    Day 2 Done

--- index.test.js
+++ index.test.js
@@ -3,20 +3,60 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
-const request = require("supertest")
+const request = require("supertest");
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician } = require('./models/index');
 const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const { seedMusician } = require("./seedData");
 
+describe('GET /musicians endpoint', () => {
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    test("Testing bakedGoods endpoint", async () => {
+        const response = await request(app).get("/musicians");
+    })
 
+    test('Should respond with status code 200', async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.statusCode).toBe(200);
+    });
 
+    test('Should return all musicians', async () => {
+        const response = await request(app).get('/musicians');
+        const responseData = JSON.parse(response.text);
 
+        expect(responseData[0]).toHaveProperty('id');
+        expect(responseData[0]).toHaveProperty('name');
+        expect(responseData[0]).toHaveProperty('instrument');
+        expect(responseData[0]).toHaveProperty('createdAt');
+        expect(responseData[0]).toHaveProperty('updatedAt');
 
-    
-})
+        expect(responseData[0].name).toBe('Mick Jagger');
+        expect(responseData[1].name).toBe('Drake');
+        expect(responseData[2].name).toBe('Jimi Hendrix');
+    });
+});
+
+describe('GET /bands endpoint', () => {
+    test('Should respond with status code 200', async () => {
+        const response = await request(app).get('/bands');
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Should return JSON data', async () => {
+        const response = await request(app).get('/bands');
+        expect(response.headers['content-type']).toContain('application/json');
+    });
+
+    test('Should return all bands', async () => {
+        const response = await request(app).get('/bands');
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData[0].name).toBe('The Beatles');
+        expect(responseData[1].name).toBe('Black Pink');
+        expect(responseData[2].name).toBe('Coldplay');
+
+        expect(responseData[0].genre).toBe('Rock');
+        expect(responseData[1].genre).toBe('Pop');
+        expect(responseData[2].genre).toBe('Rock');
+    });
+});
--- src/app.js
+++ src/app.js
@@ -1,15 +1,57 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
+
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.use(express.json())
+app.use(express.urlencoded())
 
+app.get("/musicians", async (request, response) => {
+    const data = await Musician.findAll()
+    response.json(data)
+})
 
+app.get("/musicians/:id", async (request, response) => {
+    const musiciansId = request.params.id
+    const data = await Musician.findByPk(musiciansId)
+    response.json(data)
+})
 
+app.get("/bands", async (request, response) => {
+    const data = await Band.findAll()
+    response.json(data)
+})
 
+app.get("/bands/:id", async (request, response) => {
+    const bandsId = request.params.id
+    const data = await Band.findByPk(bandsId)
+    response.json(data)
+})
+
+app.post("/musicians/", async (req, res) => {
+    const data = req.body
+    const musician = await Musician.create(req.body) 
+    res.json(musician)
+})
+
+app.put("/musicians/:id", async (req, res) => {
+    const param = req.params.id
+    const data = req.body
+    const musician = await Musician.update(data, {
+        where: {id: param}
+    }) 
+    res.json(musician)
+})
+
+app.delete("/musicians/:id", async (req, res) => {
+    const param = req.params.id
+    const musician = await Musician.destroy({where: {id: param}})
+    res.json(musician)
+})
 
 
 
